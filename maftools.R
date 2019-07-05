1e8c97a37b8ccc2f19eb6f3365b1ba92b18d6632
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-05 18:56:11 -0500

    Update maftools.R

--- maftools.R
+++ maftools.R
@@ -1,15 +1,29 @@
-install.packages("BiocManager")
-BiocManager::install("maftools")
-
-setwd("/home/shg047/work/LungBrainMetastasis/vcf")
-laml<-annovarToMaf(annovar="T1.hg19_multianno.txt", Center = NULL, refBuild = "hg19",
-             tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
-             MAFobj = F, sampleAnno = NULL)
-laml = read.maf(maf = laml)
+library("maftools")
+setwd("~/hpc/project/LungBrainMetastasis/vcf/annovar")
+annovar<-list.files(pattern="*.T1.hg19_multianno.txt")
+laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
+                   tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
+                   MAFobj = T, sampleAnno = NULL)
+getSampleSummary(laml)
+getGeneSummary(laml)
 pdf("Lung.pdf")
 plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
 dev.off()
+pdf("Lung-S1.pdf")
+oncoplot(maf = laml, top = 50,draw_titv = TRUE)
+dev.off()
+
+annovar<-list.files(pattern="*.T2.hg19_multianno.txt")
+laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
+                   tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
+                   MAFobj = T, sampleAnno = NULL)
+getSampleSummary(laml)
 getGeneSummary(laml)
-oncoplot(maf = laml, top = 10)
-oncostrip(maf = laml, genes = c('DNMT3A','NPM1', 'RUNX1'))
-? oncoplot
+pdf("Brain.pdf")
+plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+dev.off()
+pdf("Brain-S1.pdf")
+oncoplot(maf = laml, top = 50, draw_titv = TRUE)
+dev.off()
+
+
