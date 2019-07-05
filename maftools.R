fcb04dc677a8a5646fa19776289a56be6379c443
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-05 14:36:45 -0500

    Create maftools.R

---
+++ maftools.R
@@ -0,0 +1,15 @@
+install.packages("BiocManager")
+BiocManager::install("maftools")
+
+setwd("D:\\LungBrain")
+laml<-annovarToMaf(annovar="T1.hg19_multianno.txt", Center = NULL, refBuild = "hg19",
+             tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
+             MAFobj = F, sampleAnno = NULL)
+laml = read.maf(maf = laml)
+pdf("Lung.pdf")
+plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+dev.off()
+getGeneSummary(laml)
+oncoplot(maf = laml, top = 10)
+oncostrip(maf = laml, genes = c('DNMT3A','NPM1', 'RUNX1'))
+? oncoplot
