5dd60d6bd1e51a4e125771dab39cec1aa42b6bcb
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-05 19:14:00 -0500

    update
    update

--- maftools.R
+++ maftools.R
@@ -1,9 +1,10 @@
-install.packages("BiocManager")
-BiocManager::install("maftools")
-BiocManager::install("digest")
+# install.packages("BiocManager")
+# BiocManager::install("maftools")
+# BiocManager::install("digest")
 library("maftools")
 
 setwd("~/hpc/project/LungBrainMetastasis/vcf/annovar")
+setwd("C:\\Users\\shg047\\Documents\\GitHub\\LungBrainMetastasis\\annovar")
 annovar<-list.files(pattern="*.T1.hg19_multianno.txt")
 laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
                    tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
@@ -11,7 +12,7 @@ laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
 getSampleSummary(laml)
 getGeneSummary(laml)
 pdf("Lung.pdf")
-plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = T)
 dev.off()
 pdf("Lung-S1.pdf")
 oncoplot(maf = laml, top = 50,draw_titv = TRUE)
@@ -23,11 +24,27 @@ laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
                    MAFobj = T, sampleAnno = NULL)
 getSampleSummary(laml)
 getGeneSummary(laml)
+pdf("Lung_Brain.pdf")
+plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
+dev.off()
+pdf("Lung_Brain-S1.pdf")
+oncoplot(maf = laml, top = 50, draw_titv = TRUE,fontSize = 0.4)
+dev.off()
+
+? oncoplot
+
+
+annovar<-list.files(pattern="*.T*.hg19_multianno.txt")
+laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
+                   tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
+                   MAFobj = T, sampleAnno = NULL)
+getSampleSummary(laml)
+getGeneSummary(laml)
 pdf("Brain.pdf")
 plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
 dev.off()
 pdf("Brain-S1.pdf")
-oncoplot(maf = laml, top = 50, draw_titv = TRUE)
+oncoplot(maf = laml, top = 50, draw_titv = TRUE,fontSize = 0.4)
 dev.off()
 
 
