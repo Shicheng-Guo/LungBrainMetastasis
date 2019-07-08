fc094f26d9691485e02dff651b32f1bdd8e15cf8
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-07 22:26:25 -0500

    update

--- maftools.R
+++ maftools.R
@@ -8,39 +8,44 @@ install.packages("callr")
 install.packages("processx")
 install.packages("processx_3.4.0.tar.gz")
 devtools::install_github(repo = "PoisonAlien/TCGAmutations")
+BiocManager::install("BSgenome")
+library("BSgenome", quietly = TRUE)
 
 
 setwd("~/hpc/project/LungBrainMetastasis/vcf/annovar")
-setwd("C:\\Users\\shg047\\Documents\\GitHub\\LungBrainMetastasis\\annovar")
+setwd("C:\\Users\\shg047\\Documents\\GitHub\\LungBrainMetastasis\\annovar\\multianno")
 
 ### Lung
-annovar<-list.files(pattern="*.T1.hg19_multianno.txt")
+annovar<-list.files(pattern="*_T1.hg19_multianno.txt")
 laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
                    tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
                    MAFobj = T, sampleAnno = NULL)
+Lung<-laml
 getSampleSummary(laml)
 getGeneSummary(laml)
 pdf("Lung.pdf")
 plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = T)
 dev.off()
 pdf("Lung-S1.pdf")
-oncoplot(maf = laml, top = 50,draw_titv = TRUE)
+oncoplot(maf = laml, top = 50,draw_titv = TRUE,fontSize = 0.8)
 dev.off()
 pdf("Lung-S2.pdf")
 somaticInteractions(maf = laml, top = 25, pvalue = c(0.05, 0.1))
 dev.off()
 
+
 ### Brain
-annovar<-list.files(pattern="*.T2.hg19_multianno.txt")
+annovar<-list.files(pattern="*_T2.hg19_multianno.txt")
 laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
                    tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
                    MAFobj = T, sampleAnno = NULL)
+Brain<-laml
 getSampleSummary(laml)
 getGeneSummary(laml)
-pdf("Lung_Brain.pdf")
+pdf("Brain.pdf")
 plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
 dev.off()
-pdf("Lung_Brain-S1.pdf")
+pdf("Brain-S1.pdf")
 oncoplot(maf = laml, top = 50, draw_titv = TRUE,fontSize = 0.4)
 dev.off()
 pdf("Brain-S2.pdf")
@@ -48,7 +53,7 @@ somaticInteractions(maf = laml, top = 25, pvalue = c(0.05, 0.1))
 dev.off()
 
 ### Lung + Brain
-annovar<-list.files(pattern="*.T*.hg19_multianno.txt")
+annovar<-list.files(pattern="*.hg19_multianno.txt")
 laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
                    tsbCol = NULL, table = "refGene", basename = NULL, sep = "\t",
                    MAFobj = T, sampleAnno = NULL)
@@ -58,11 +63,40 @@ pdf("Lung_Brain.pdf")
 plotmafSummary(maf = laml, rmOutlier = TRUE, addStat = 'median', dashboard = TRUE, titvRaw = FALSE)
 dev.off()
 pdf("Lung_Brain-S1.pdf")
-oncoplot(maf = laml, top = 50, draw_titv = TRUE,fontSize = 0.4)
+oncoplot(maf = laml, top = 50, sampleOrder=SO,removeNonMutated=F,draw_titv = TRUE,fontSize = 0.4,showTumorSampleBarcodes=T)
 dev.off()
+
 pdf("Lung_Brain-S2.pdf")
 somaticInteractions(maf = laml, top = 25, pvalue = c(0.05, 0.1))
 dev.off()
+
+
+SO<-unlist(lapply(annovar,function(x) unlist(strsplit(x,"[.]"))[1]))
+oncoplot(maf = laml, top = 50, sampleOrder=SO,removeNonMutated=F,draw_titv = TRUE,fontSize = 0.4,showTumorSampleBarcodes=T)
+oncoplot(maf = laml, top = 50, draw_titv = TRUE,fontSize = 0.4)
+
+? oncoplot
+genes = c("KMT2C", "BAGE2", "ANKRD36C", "AHNAK2", "ADAMTSL4","MST1L","PRAMEF4","PDE4DIP","FLT3LG","DMBT1")
+coOncoplot(m1 = Lung, m2 = Brain, m1Name = 'Lung', m2Name = 'Brain', genes = genes, removeNonMutated = TRUE)
+
+
+library('NMF')
+BSgenome::available.genomes()
+BiocManager::install("BSgenome.Hsapiens.UCSC.hg19")
+
+
+library(BSgenome.Hsapiens.UCSC.hg19, quietly = TRUE)
+
+library('pheatmap')
+
+laml.tnm= trinucleotideMatrix(maf = Lung, prefix = 'chr', add = TRUE, ref_genome = "BSgenome.Hsapiens.UCSC.hg19")
+laml.sign = extractSignatures(mat = laml.tnm, nTry = 6, plotBestFitRes = FALSE)
+plotSignatures(laml.sign, title_size = 0.8, )
+pheatmap::pheatmap(mat = laml.sign$coSineSimMat, cluster_rows = FALSE, main = "cosine similarity against validated signatures")
+laml.se = signatureEnrichment(maf = laml, sig_res = laml.sign)
+
+
+
 
 annovar<-list.files(pattern="*.T*.hg19_multianno.txt")
 laml<-annovarToMaf(annovar=annovar, Center = NULL, refBuild = "hg19",
