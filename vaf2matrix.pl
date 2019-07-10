9a3278a0842fdfcaa54ee2ad006b874beab1386c
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-09 20:45:52 -0400

    Create vaf2matrix.pl

---
+++ vaf2matrix.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+chdir "/home/shg047/work/wes/LungBrainMetastasis/vaf";
+my @file=glob("*vafs");
+
+my %data;
+my %gene;
+my %sam;
+my %pos;
+
+foreach my $file(@file){
+open F,$file;
+my($sam)=split/.vafs/,$file;
+while(<F>){
+my @line=split/\s+/;
+my $pos="$line[0]:$line[1]";
+$pos{$pos}=$pos;
+$sam{$sam}=$sam;
+$data{$sam}{$pos}=$line[9];
+}
+}
+
+my $sam=join("\t",sort keys %sam);
+print "\t$sam\n";
+foreach my $pos(sort keys %pos){
+print "$pos";
+foreach my $sam(sort keys %sam){
+if(defined $data{$sam}{$pos}){
+print "\t$data{$sam}{$pos}";
+}else{
+print "\t0";
+}
+}
+print "\n";
+}
