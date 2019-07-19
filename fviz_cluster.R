cd990cd3782914e752f2508c22a285ccf35becf6
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-07-18 21:05:54 -0500

    Update fviz_cluster.R

--- fviz_cluster.R
+++ fviz_cluster.R
@@ -1,4 +1,4 @@
-setwd("//mcrfnas2/bigdata/Genetic/Projects/shg047/project/LungBrainMetastasis/vaf")
+setwd("~/hpc/project/LungBrainMetastasis")
 library("factoextra")
 data<-read.table("vafmatrix.txt",head=T,check.names = F)
 head(data)
@@ -9,6 +9,7 @@ for(i in 1:14){
   km.res <- kmeans(temp,8, nstart = 25)
   write.table(km.res$cluster,file=paste(name[2*i-1],".cluster8.txt",sep=""),sep="\t",col.names = F,row.names = T,quote=F)
   p<-fviz_cluster(km.res, stand=F,data = temp,choose.vars = colnames(temp),geom = c("point"),pointsize = 2)+theme_bw()
+  p<-p+xlim(0,100)+ylim(0,100)
   print(p)
   ggsave(p,file=paste(name[2*i-1],".8.pdf",sep=""))
   print(name[2*i-1])
